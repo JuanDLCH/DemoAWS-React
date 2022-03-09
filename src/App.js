63d69b4643e61a08d5d9818f428f119101edaa5f
JuanDLCH <juandiego0329@gmail.com>
2022-03-09 17:05:31 -0500

    ip

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   }, [])
 
   const getData = () => {
-    fetch('http://localhost:3001/sapos')
+    fetch('http://172.31.81.213:3001/sapos')
       .then(response => response.json())
       .then(data => setData(data))
       .catch(err => console.error(err))
@@ -22,7 +22,7 @@ function App() {
     const note = {
       nombre: textAreaValue
     }
-    axios.post('http://localhost:3001/sapos', note)
+    axios.post('http://172.31.81.213:3001/sapos', note)
     setTextAreaValue('')
     getData()
   }
