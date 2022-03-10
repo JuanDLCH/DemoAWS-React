62322ef4647aed670154607d3ee322ef59b95add
JuanDLCH <juandiego0329@gmail.com>
2022-03-09 21:48:29 -0500

    tiempo

--- src/App.js
+++ src/App.js
@@ -32,7 +32,41 @@ function App() {
 
   const handleOnChange = (e) => {
     setTextAreaValue(e.target.value)
+    getData()
   }
+
+  function tick() {
+    const element = (
+      <div>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <center>
+          <div className="badge bg-primary text-wrap">
+            <h1>Deja un mensaje para tus amigos :3</h1>
+          </div>
+          <ul className="list-group">
+            {
+              data &&
+              data.map(note => (
+                <li className="list-group-item" key={note.id}><p><strong>- {note.nombre} - <br></br></strong></p></li>
+              ))
+            }
+          </ul>
+          <form >
+            <textarea name='nombre' onChange={handleOnChange} value={textAreaValue}></textarea> <br></br>
+            <button type='submit' className="btn btn-primary" onClick={handleClick}>Agregar</button>
+          </form> </center>
+      </div>
+    );
+    ReactDOM.render(
+      element,
+      document.getElementById('root')
+    );
+  }
+
+  setInterval(tick, 1000);
 
   return (
     <div>
@@ -41,21 +75,21 @@ function App() {
       <br></br>
       <br></br>
       <center>
-      <div className="badge bg-primary text-wrap">
-            <h1>Deja un mensaje para tus amigos :3</h1>
-      </div>
-      <ul className="list-group">
-        {
-          data &&
-          data.map(note => (
-            <li className="list-group-item" key={note.id}><p><strong>- {note.nombre} - <br></br></strong></p></li>
-          ))
-        }
-      </ul>
-      <form >
-        <textarea name='nombre' onChange={handleOnChange} value={textAreaValue}></textarea> <br></br>
-        <button type='submit' className="btn btn-primary" onClick={handleClick}>add</button>
-      </form> </center>
+        <div className="badge bg-primary text-wrap">
+          <h1>Deja un mensaje para tus amigos :3</h1>
+        </div>
+        <ul className="list-group">
+          {
+            data &&
+            data.map(note => (
+              <li className="list-group-item" key={note.id}><p><strong>- {note.nombre} - <br></br></strong></p></li>
+            ))
+          }
+        </ul>
+        <form >
+          <textarea name='nombre' onChange={handleOnChange} value={textAreaValue}></textarea> <br></br>
+          <button type='submit' className="btn btn-primary" onClick={handleClick}>Agregar</button>
+        </form> </center>
     </div>
   );
 }
