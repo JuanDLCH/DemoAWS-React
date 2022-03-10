ee6845cd0873aec35c98c39970eacb86202f0976
JuanDLCH <juandiego0329@gmail.com>
2022-03-09 21:51:05 -0500

    time

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import ReactDOM from 'react-dom'
+import React from 'react'
 
 function App() {
   const [data, setData] = useState(null)
